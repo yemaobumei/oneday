a3a17d29a661b42cf7b0bba9ccb6a07add38ac2e
YeFan <yefan@YeFandeMBP.lan>
2016-12-09 01:46:22 +0800

    排行榜单

--- qf/ye.py
+++ qf/ye.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 import requests,re,time
 path=r"./"#数据存放路径
-starTime=time.time()
+
+
+
+
 #手机app访问头文件
 headers={
 'Host': 'client.tyread.com:8080',
@@ -30,47 +33,60 @@ cookies={
 	'user':"{\"isGuest\":true,\"sessionId\":\"DEB5BCD0BADDE53DD5C9A5E85804F653\",\"userGuestId\":\"1481041410655068\",\"viewType\":1,\"userType\":\"4\"}"
 }
 
-#获取主编力荐小说id
 
-urlTuijianList=r'http://clientwap.tyread.com/androidFive/tuijianList.action?recCtrlId=146645&listName=%E4%B8%BB%E7%BC%96%E5%8A%9B%E8%8D%90&ca=iphone'
-idTuijianList=re.findall('data-bookid=\"(\d+)\"',requests.get(urlTuijianList).content)
-
-#print idTuijianList
-urlBookDetail=r'http://client.tyread.com:8080/portalapi/portalapi?comment=3&contentId=###&mode=1&needMonthInfo=1'
-#open(path+'info.html','wb').write(requests.get(url,headers=headers).content)
-
-#纪录榜单小说详细信息
-def top():
-	f=open(path+'TuijianList.txt','wb')
+ 
+#每天循环统计数据
+def loop(times=30):
+	#打开纪录文档
+	f_Tuijian=open(path+'TuijianList.txt','wb')
 	f_descri=open(path+'description.txt','wb')
 	error=open(path+'error.txt','wb')
-	for id in idTuijianList:
-		f.write(time.strftime("%Y-%m-%d ", time.localtime())+'\n')
-		tempUrl=re.sub('###',id,urlBookDetail)
-		regx=r'<contentID>(.*?)</contentID><contentName>(.*?)</contentName><catalogName>(.*?)</catalogName>.*?<authorName>(.*?)</authorName>.*?<description>([\s\S]*?)</description><count>(.*?)</count>'
-		#[小说id，小说名，类别，作者，小说简介，阅读量]
-		while(True):
-			try:
-				s=requests.session()
-				s.keep_alive=False
-				content=s.get(tempUrl,headers=headers).content
-				line=re.findall(regx,content)[0]
-				t='{0: <40} {1: <20} {2: <20} {3: <10}'.format(line[1],line[2],line[3],line[5])
-				f.write(str(t)+'\n')
-				f_descri.write(line[0]+'\n'+line[-2]+'\n')
-				break;
-			except IndexError:
-				print 'indexerror'
-				error.write(content+'\n\n')
-			except Exception as e:
-				error.write(id+':')
-				error.write(str(e.message))
-				error.write('\n\n')
-				print id,e
-			time.sleep(10)
-			
-	f.close()
+	#获取主编力荐小说id
+	urlTuijianList=r'http://clientwap.tyread.com/androidFive/tuijianList.action?recCtrlId=146645&listName=%E4%B8%BB%E7%BC%96%E5%8A%9B%E8%8D%90&ca=iphone'
+	idTuijianList=re.findall('data-bookid=\"(\d+)\"',requests.get(urlTuijianList).content)
+	urlBookDetail=r'http://client.tyread.com:8080/portalapi/portalapi?comment=3&contentId=###&mode=1&needMonthInfo=1'
+	#纪录榜单小说详细信息
+	def top():
+		date=time.strftime("%Y-%m-%d ", time.localtime())
+		#打开每日推荐榜单纪录文件
+		f_day=open(path+date+'Tuijian.txt','wb')
+		for id in idTuijianList:
+			tempUrl=re.sub('###',id,urlBookDetail)
+			regx=r'<contentID>(.*?)</contentID><contentName>(.*?)</contentName><catalogName>(.*?)</catalogName>.*?<authorName>(.*?)</authorName>.*?<description>([\s\S]*?)</description><count>(.*?)</count>'
+			#[小说id，小说名，类别，作者，小说简介，阅读量]
+			while(True):
+				try:
+					s=requests.session()
+					s.keep_alive=False
+					content=s.get(tempUrl,headers=headers).content
+					line=re.findall(regx,content)[0]
+					t='{0: <40} {1: <20} {2: <20} {3: <10}'.format(line[1],line[2],line[3],line[5])
+					f_Tuijian.write(str(t)+'\n')
+					f_day.write(str(t)+'\n')
+					f_descri.write(line[0]+'  '+line[1]+'\n'+line[-2]+'\n')
+					break;
+				except IndexError:
+					print 'indexerror'
+					error.write(content+'\n\n')
+				except Exception as e:
+					error.write(id+':')
+					error.write(str(e.message))
+					error.write('\n\n')
+					print id,e
+				time.sleep(10)				
+		f_day.close()
+
+	for i in range(times):
+		f_Tuijian.write(time.strftime("%Y-%m-%d ", time.localtime())+'\n')
+		f_descri.write(time.strftime("%Y-%m-%d ", time.localtime())+'\n')
+		startTime=time.time()
+		top()
+		endTime=time.time()
+		#time.sleep(24*60*60-endTime+startTime)
+		#time.sleep(20)	
+	f_Tuijian.close()
 	f_descri.close()
+	error.close()
 
 #高频词汇分析
 def word_frequency():
@@ -88,13 +104,27 @@ def word_frequency():
 		#print isinstance(word,unicode) True
 		t='{0:#<20}{1:->10} '.format(word.encode('utf-8'),str(freq))
 		freqFile.write(str(t)+'\n')
+	freqFile.close()
 
-
-endTime=time.time()
+def popular():
+	headers={
+	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+	'Accept-Encoding': 'gzip, deflate'
+	}
+	recorder=open(r'./recoder.html','wb')
+	url=r'http://wap.tyread.com/pindao/shucheng/paihang/index.html?is_ctwap=0&y=0&id=749&q=1&pageNo=#'
+	for i in range(1):
+		desUrl=re.sub('#',str(i+1),url)
+		regx=r'li[\s\S]*?bookdetail/(\d+?)/gobook'
+		response=requests.get(url,headers=headers)
+		if response.status_code==200:
+			print (re.findall(regx,response.content))
+			recorder.write(response.content)
 if __name__ == '__main__':
-	#top()
-	word_frequency()
-#time.sleep(24*60*60-endTime+startTime)
+	#loop(1)
+	#word_frequency()
+	popular()
+
 
 #主编力鉴：
 #http://clientwap.tyread.com/androidFive/tuijianList.action?recCtrlId=146645&listName=%E4%B8%BB%E7%BC%96%E5%8A%9B%E8%8D%90&ca=iphone
