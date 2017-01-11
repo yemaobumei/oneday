8143237f21f64032ba7f517943426c3306c3f668
yemaobumei <385131462@qq.com>
2017-01-11 22:25:46 +0800

    dream

--- 梦想/dream.js
+++ 梦想/dream.js
@@ -1,16 +1,18 @@
 var settings={
 	operation: "GET",
 	headers:{
-		'Host': 'h5.weiyingonline.com',
-		'Connection': 'keep-alive',
-		'Cache-Control': 'max-age=0',
-		'Upgrade-Insecure-Requests': '1',
-		'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-		'Accept-Encoding': 'gzip, deflate, sdch',
-		'Accept-Language': 'zh-CN,zh;q=0.8',
+			'Host': 'h5.weiyingonline.com',
+			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+			'Connection': 'keep-alive',
+			'Upgrade-Insecure-Requests': '1',
+			'Cookie': 'totoro_cookie=1484129147647_9300; totoro_share_click_cookie=1484129147638_1808; JSESSIONID=685BEB546B16CF9F86BA068B39BBA22D',
+			'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_1_1 like Mac OS X) AppleWebKit/602.2.14 (KHTML, like Gecko) Mobile/14B100 MicroMessenger/6.5.3 NetType/WIFI Language/zh_CN',
+			'Accept-Language': 'zh-cn',
+			'Accept-Encoding': 'gzip, deflate',
+			'Connection': 'keep-alive',
 		}
 }
+
 DESKTOP_USER_AGENTS = [ "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
 		'Mozilla/5.0 (X11; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0',
 		'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36',
@@ -34,18 +36,18 @@ var i=0;
 //var phantom = require('phantom');
 var webPage = require('webpage');
 var page = webPage.create();
-var url='http://h5.weiyingonline.com/share_play/SharePlayClickAction.a?shareDetailId=1170853'
+var url='http://h5.weiyingonline.com/share_play/SharePlayClickAction.a?shareDetailId=1181700'
 function iter(){
 	//随机产生user-agent
-	settings.headers['User-Agent']=DESKTOP_USER_AGENTS[Math.floor(Math.random()*7)]
+	//settings.headers['User-Agent']=DESKTOP_USER_AGENTS[Math.floor(Math.random()*7)]
 	var page=null;
 	page = webPage.create();
 	page.open(url, settings,function (s) {
 	  console.log(s);
 	});
 	i=i+1;
-	console.log(i,settings.headers['User-Agent']);
-	if(i>=2000){phantom.exit();}
-	setTimeout(arguments.callee, 2000)
+	console.log(i);
+	if(i>=1000){phantom.exit();}
+	setTimeout(arguments.callee, 3000)
 }
 iter()
