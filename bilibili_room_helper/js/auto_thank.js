f70355e42fccee543f7f1c0d40a036f0b4452721
yemaobumei <385131462@qq.com>
2017-05-05 00:55:28 +0800

    只能

--- bilibili_room_helper/js/auto_thank.js
+++ bilibili_room_helper/js/auto_thank.js
@@ -27,7 +27,6 @@ var observer = new MutationObserver(function(mutations) {
     		welcome($node);	
     	}else if($node.find('.chat-msg').length>0) {
     		reply($node);
-    		console.log(1)
     	}   	
     });
   });    
@@ -37,7 +36,6 @@ var observer = new MutationObserver(function(mutations) {
 var gift_num=0;//记录礼物赠送次数
 var  single_gift={};//{'x':1,'y':2}记录单个礼物赠送次数，提示不要刷屏
 function thank($giftMsg,uname,giftCount,action) {
-	gift_num+=1;
 	if(gift_num%20==0){single_gift={}};
 	if($giftMsg.length>0) {
 		var uname=$giftMsg.find(uname).text();
@@ -76,18 +74,36 @@ function reply($node){
 	if(gift_num%40==0){sendDanmu("主播游戏这么厉害，不上波船吗。")};
 	var uname=$node.find('.user-name').text();
 	var msg=$node.find('.msg-content').text();
-	console.log(msg)
-	var database={'背景音乐':'左上角网易云关注有歌单','bgm':'左上角网易云关注有歌单','666':'主播这么厉害不点拨关注吗',
-	  '胸':'请大家注意弹幕礼仪!','鸡':'请大家注意弹幕礼仪','菜':'这只是发挥失常，大家看久点就知道了。',
-	  '段位':'主播钻石水平，日常AD。想看什么英雄可以跟主播说。'	
+	if(uname=='[自己] : '){
+		return;
+	}
+	var database={'背景音乐':'网易云ID:喵咭喵呜QAQ,关注有歌单','求bgm':'网易云ID:喵咭喵呜QAQ,关注有歌单','喵咭6666':'主播这么厉害不点拨关注吗',
+	  '主播胸小':'请大家注意弹幕礼仪!','做鸡巴儿':'请大家注意弹幕礼仪','主播真菜':'这只是发挥失常，大家看久点就知道了',
+	  '主播段位':'主播钻石水平，日常AD。想看什么英雄可以跟主播说',
+	  '主播炸了':'主播加油，胜利在望，决不动摇',
+	  '夜猫大佬gay':'不要趁我不在就gay我',
+	  'gay弹幕姬':'不要调戏我，会坏的',
+	  '小姐姐漂亮':'喵咭和往常一样漂亮',
+	  '主播漂亮':'喵咭和往常一样漂亮',
+	  '主播修仙':'不要休闲哟，对身体不好呢',
+	  '唱歌好听':'喵咭唱歌好听',
+	  '废话在哪里呢':'废话被麻麻gank了',
+	  '夜猫大佬不在了':'我与喵咭共存亡',	
 	}
 	$.each(database,function(key,val){
 		if(msg.search(key)>-1){
 			sendDanmu(val);
+			return;			
+		}else if(compare(key.split(''),msg.split(''))>=45.0){
+			sendDanmu(val);
+			return;	
 		}
+
 	})
+}
 //发送弹幕消息
 function sendDanmu(danmu)	{
+	gift_num+=1;
 	$('#danmu-textbox').val(danmu);
 	$('#danmu-send-btn').click();
 }
@@ -100,3 +116,29 @@ observer.observe(target, config);
  
 // 随后,你还可以停止观察
 //observer.disconnect();
+
+
+
+
+function compare(x, y) {
+    var z = 0;
+    var s = x.length + y.length;;
+ 
+    x.sort();
+    y.sort();
+    var a = x.shift();
+    var b = y.shift();
+ 
+    while(a !== undefined && b !== undefined) {
+        if (a === b) {
+            z++;
+            a = x.shift();
+            b = y.shift();
+        } else if (a < b) {
+            a = x.shift();
+        } else if (a > b) {
+            b = y.shift();
+        }
+    }
+    return z/s * 200;
+}
