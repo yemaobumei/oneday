70f47e26ae55f35f3bd9ca394eac2fbd247874a1
yemaobumei <385131462@qq.com>
2017-05-08 23:15:59 +0800

    roomid非法字符

--- bilibili/bilibili_danmuji/DanmuWebsocket.py
+++ bilibili/bilibili_danmuji/DanmuWebsocket.py
@@ -46,7 +46,7 @@ class DanmuWebsocket():
 		self._UserCount = 0
 		self._ChatHost = 'livecmt-1.bilibili.com'
 
-		self._roomId =  "1273106"　
+		self._roomId = "1273106"
 		self._roomId = int(self._roomId)
 
 		self.cookies=cookies
