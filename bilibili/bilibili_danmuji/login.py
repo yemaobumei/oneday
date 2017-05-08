1d5206505799b4a06b3fdce2e369344ea8b35db1
yemaobumei <385131462@qq.com>
2017-05-08 17:16:01 +0800

    删除cap.png

--- bilibili/bilibili_danmuji/login.py
+++ bilibili/bilibili_danmuji/login.py
@@ -4,8 +4,8 @@
 from api import Client
 
 #登录B站获取cookies
-username=input('请输入用户名')
-password=input('请输入密码')
+username=input('请输入用户名: ')
+password=input('请输入密码: ')
 LoginClient=Client(username,password)
 cookies=LoginClient.cookies_login()
 while not LoginClient.isLogin:
