d4d4b5a5c20e2be9ce385edd034cc6159a610a6c
yemaobumei <385131462@qq.com>
2017-05-19 18:25:03 +0800

    检查风暴数据库

--- bilibili/fengbao/tv.py
+++ bilibili/fengbao/tv.py
@@ -1,8 +1,8 @@
 from sql import *
 # addSmallTv(1222,1,1)
 session = DBSession()
-for instance in session.query(Fengbao).filter_by(realRoomid=1589452).all():#filter_by(fansnum=0): 
-	print(instance.realRoomid,instance.send_uname)
+for instance in session.query(Fengbao).order_by(Fengbao.date):#filter_by(fansnum=0): 
+	print(instance.realRoomid,instance.send_uname,instance.date)
 	#session.delete(instance)
 session.commit()
 session.close()
