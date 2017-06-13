479bedea71869dcbbd0ffd9b438d63d895045d13
yemaobumei <385131462@qq.com>
2017-06-14 00:16:10 +0800

    直播脚本

--- bilibili/zhibo.sh
+++ bilibili/zhibo.sh
@@ -8,6 +8,7 @@ live_url=$(echo $live_info|cut -d '[' -f 3|cut -d ']' -f 1)
 #循环推送直播流
 while true
 do
+ps -ef|grep 'ffmpeg'|awk '{print$2}'|xargs kill -9
 status=$(curl "http://api.live.bilibili.com/live/getInfo?roomid=${roomid}"|sed -e 's/[{}]/''/g'|awk -v RS=',"' -F: '/^_status/{print $2}')
 #if [[ $live_info =~ 'CDATA' ]]
 
@@ -24,3 +25,10 @@ else
 fi
 done
 
+
+
+
+#ffmpeg -i out.mp4 -vf "drawtext=text='lihuibin':fontfile=./fonts/msyh.ttf:fontsize=24:fontcolor=red@0.8:x=w-tw-20:y=h-th-20" -c:v libx264 -c:a copy output8.mp4
+#ffmpeg -re -i "$live_url"  -vf "drawtext=text='房间号1273106，主播喵咭喵呜':fontfile=./fonts/msyh.ttf:fontsize=34:fontcolor=yellow@0.8:x=w-text_w:y=text_h"  -vcodec libx264 -acodec copy -b:a 100k -f flv "rtmp://xl.live-send.acg.tv/live-xl/?streamname=live_25303175_5745325&key=71771fb7bb37ccbdc17888f178f56075"
+
+ffmpeg -re -i "movie/LoveLett.mp4" -vf "drawtext=text='电影名：情书':fontfile=./fonts/msyh.ttf:fontsize=18:fontcolor=white@0.8:x=w-tw-20:y=th" -vcodec libx264 -acodec copy -b:a 128k -f flv "rtmp://xl.live-send.acg.tv/live-xl/?streamname=live_25303175_5745325&key=71771fb7bb37ccbdc17888f178f56075"
