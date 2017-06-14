b4da17bc34639982727530c7d0176024c8e82840
yemaobumei <385131462@qq.com>
2017-06-14 18:29:59 +0800

    自己直播间弹幕姬

--- bilibili/bilibili_danmuji/yemao/danmuji.py
+++ bilibili/bilibili_danmuji/yemao/danmuji.py
@@ -18,9 +18,7 @@ from api import Client
 # roomid="1273106"
 # #roomid="2570641"
 info = [
-	{'username':'13126772351','password':'ye06021123','roomid':1273106},
-	{'username':'979365217@qq.com','password':'ye06021123','roomid':4416185},
-	{'username':'13390776820','password':'wsglr3636...','roomid':4416185}
+	{'username':'13126772351','password':'ye06021123','roomid':2570641}
 ]
 tasks = []
 for each in info:		
--- bilibili/zhibo.sh
+++ bilibili/zhibo.sh
@@ -31,4 +31,6 @@ done
 #ffmpeg -i out.mp4 -vf "drawtext=text='lihuibin':fontfile=./fonts/msyh.ttf:fontsize=24:fontcolor=red@0.8:x=w-tw-20:y=h-th-20" -c:v libx264 -c:a copy output8.mp4
 #ffmpeg -re -i "$live_url"  -vf "drawtext=text='房间号1273106，主播喵咭喵呜':fontfile=./fonts/msyh.ttf:fontsize=34:fontcolor=yellow@0.8:x=w-text_w:y=text_h"  -vcodec libx264 -acodec copy -b:a 100k -f flv "rtmp://xl.live-send.acg.tv/live-xl/?streamname=live_25303175_5745325&key=71771fb7bb37ccbdc17888f178f56075"
 
-ffmpeg -re -i "movie/LoveLett.mp4" -vf "drawtext=text='电影名：情书':fontfile=./fonts/msyh.ttf:fontsize=18:fontcolor=white@0.8:x=w-tw-20:y=th" -vcodec libx264 -acodec copy -b:a 128k -f flv "rtmp://xl.live-send.acg.tv/live-xl/?streamname=live_25303175_5745325&key=71771fb7bb37ccbdc17888f178f56075"
+#转码直播
+#ffmpeg -i Love.Letter.1995.BluRay.iPad.720p.x264.AAC.3Audio-NYPAD.mp4  -b 200k -s 640x320 out.mp4
+#ffmpeg -re -i "./movie/LoveLett.mp4" -vf "drawtext=text='电影名：情书':fontfile=./fonts/msyh.ttf:fontsize=18:fontcolor=white@0.8:x=w-tw-20:y=th" -vcodec libx264 -acodec copy -b:a 128k -f flv "rtmp://xl.live-send.acg.tv/live-xl/?streamname=live_25303175_5745325&key=71771fb7bb37ccbdc17888f178f56075"
