1a895ffdea167dea82203339cd7ffb0a63349dec
YeFan <yefan@YeFandeMBP.lan>
2017-07-04 13:30:29 +0800

    柠檬风暴nfb

--- bilibili/fengbao/ningmengFengbao/ntime.sh
+++ bilibili/fengbao/ningmengFengbao/ntime.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 while true
 do
-	nohup python3 ningmengfengbao.py >fengbao.out 2>&1 &
-	sleep 30m && ps -ef|grep 'python3'|grep 'ningmengfengbao'|awk '{print$2}'|xargs kill -9 && echo -e `date`
+	nohup python3 nfb.py >fengbao.out 2>&1 &
+	sleep 30m && ps -ef|grep 'python3'|grep 'nfb'|awk '{print$2}'|xargs kill -9 && echo -e `date`
 done
 
