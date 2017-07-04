df30048bb9150b0b6e4c9f64f76d81a879c5f6c5
YeFan <yefan@YeFandeMBP.lan>
2017-07-04 12:08:58 +0800

    柠檬风暴名字修改

--- bilibili/fengbao/ningmengFengbao/time.sh
+++ bilibili/fengbao/ningmengFengbao/time.sh
@@ -2,6 +2,6 @@
 while true
 do
 	nohup python3 fengbao.py >fengbao.out 2>&1 &
-	sleep 30m && ps -ef|grep 'python3'|grep 'fengbao'|awk '{print$2}'|xargs kill -9 && echo -e `date`
+	sleep 30m && ps -ef|grep 'python3'|grep 'ningmengfengbao'|awk '{print$2}'|xargs kill -9 && echo -e `date`
 done
 
